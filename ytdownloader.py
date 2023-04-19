c9ebaf0f4e85698cd3e41b7b81e53bac6abdacd8
Hafid Ikhsan Arifin <harifin315@gmail.com>
2023-04-19 10:36:55 +0700

    Add argparse start, end and version ytdownloader.py

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,8 +1,14 @@
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--url", required=True,
-	help="youtube link url")
+
+ap.add_argument("-u", "--url", required=True, help="youtube link url")
+ap.add_argument("-s", "--start", required=True, default="00:00", help="start duration")
+ap.add_argument("-e", "--end", required=True, default="00:10", help="start duration")
+ap.add_argument("-v", "--version", action="version", version="%(prog)s 1.0.0")
+
 args = vars(ap.parse_args())
 
 print("Youtube link {}".format(args["url"]))
+print("Start time {}".format(args["start"]))
+print("End time {}".format(args["end"]))
